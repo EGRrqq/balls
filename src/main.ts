9796a774bf7e815e6e5cc81d2d8bb90d22203cbb
EGRrqq <egrrqqdev@gmail.com>
2024-03-16 09:31:15 -0400

    feat: redo proto based on mdn article https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Advanced_animations
    - ball moves based on mouseover pos
    - ball moves on mouse click pos
    - ball have a little impulse
    - collision detection on canvas edges

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,84 @@
-import { Ball } from './controllers/Ball';
-import './style.css'
+import "./style.css";
 
-window.addEventListener("load", init, { once: true });
+const board = document.getElementById("board") as HTMLCanvasElement;
+const ctx = board!.getContext("2d");
+let raf: number;
 
-function init() {
-  const board = document.getElementById("board") as HTMLCanvasElement;
-  const ctx = board.getContext("2d");
+const ball = {
+  x: 100,
+  y: 100,
+  vx: 5,
+  vy: 1,
+  radius: 50,
+  color: "blue",
+  draw() {
+    ctx!.beginPath();
+    ctx!.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx!.closePath();
+    ctx!.fillStyle = this.color;
+    ctx!.fill();
+  },
+};
 
-  if (!ctx) return;
-
-  const ball = new Ball(ctx);
-  ball.draw({
-    posX: 100,
-    posY: 75,
-  });
+function clear() {
+  ctx!.clearRect(0, 0, board.width, board.height);
 }
+
+function draw(e: MouseEvent) {
+  clear();
+  ball.draw();
+
+  const mouseX = e.clientX - board.getBoundingClientRect().left;
+  const mouseY = e.clientY - board.getBoundingClientRect().top;
+
+  // Calculate the direction vector from circle center to mouse
+  const dirX = mouseX - ball.x;
+  const dirY = mouseY - ball.y;
+  const distance = Math.sqrt(dirX * dirX + dirY * dirY);
+
+  // Normalize the direction vector
+  const normalizedDirX = dirX / distance;
+  const normalizedDirY = dirY / distance;
+
+  // Apply an impulse
+  const impulseStrength = 0.7;
+  ball.vx += normalizedDirX * impulseStrength;
+  ball.vy += normalizedDirY * impulseStrength;
+
+  ball.x += ball.vx;
+  ball.y += ball.vy;
+  ball.vx *= 0.99;
+  ball.vy *= 0.99;
+
+  // Detect collision
+  if (
+    ball.y + ball.vy > board.height - ball.radius ||
+    ball.y + ball.vy < ball.radius
+  ) {
+    ball.vy = -ball.vy;
+  }
+  if (
+    ball.x + ball.vx > board.width - ball.radius ||
+    ball.x + ball.vx < ball.radius
+  ) {
+    ball.vx = -ball.vx;
+  }
+
+  console.log({ x: ball.x, y: ball.y });
+
+  raf = window.requestAnimationFrame(() => draw(e));
+}
+
+board.addEventListener("click", (e) => {
+  raf = window.requestAnimationFrame(() => draw(e));
+});
+
+board.addEventListener("mouseover", (e) => {
+  raf = window.requestAnimationFrame(() => draw(e));
+});
+
+board.addEventListener("mouseout", () => {
+  window.cancelAnimationFrame(raf);
+});
+
+ball.draw();
