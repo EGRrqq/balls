d8da3ddb4b246f655c8bc31360f478da2ce4f31c
EGRrqq <egrrqqdev@gmail.com>
2024-03-18 07:21:54 -0400

    feat: add a check for data

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,42 @@
-import { IData, generateData } from "./data";
 import "./style.css";
+import { IData, generateData } from "./data";
 
 const data = generateData(10);
 
-window.addEventListener("load", () => draw(data!), {
-  once: true,
-});
-data!.boardController.board.addEventListener("mousemove", (e) =>
-  onMove(e, data!)
-);
+if (data) {
+  window.addEventListener("load", () => draw(data), {
+    once: true,
+  });
+  data.boardController.board.addEventListener("mousemove", (e) =>
+    onMove(e, data)
+  );
 
-function onMove(e: MouseEvent, data: IData) {
-  data.shapeControllers.forEach((c) => {
-    const dirVec = c.vector.dirVec(
-      c.mouse.getPos(e, data.boardController.board),
-      c.ball.getPos()
-    );
-    const distance = c.vector.distance(dirVec);
-    const normDirVec = c.vector.normalizeDirVec(dirVec, distance);
+  function onMove(e: MouseEvent, data: IData) {
+    data.shapeControllers.forEach((c) => {
+      const dirVec = c.vector.dirVec(
+        c.mouse.getPos(e, data.boardController.board),
+        c.ball.getPos()
+      );
+      const distance = c.vector.distance(dirVec);
+      const normDirVec = c.vector.normalizeDirVec(dirVec, distance);
 
-    c.ball.onTouch(distance, () => {
-      c.ball.applyImpulse(-8, normDirVec);
+      c.ball.onTouch(distance, () => {
+        c.ball.applyImpulse(-8, normDirVec);
+      });
     });
-  });
-}
+  }
 
-function draw(data: IData) {
-  data.boardController.clearBoard();
+  function draw(data: IData) {
+    data.boardController.clearBoard();
 
-  data.shapeControllers.forEach((c) => {
-    c.ball.draw();
+    data.shapeControllers.forEach((c) => {
+      c.ball.draw();
 
-    c.ball.updatePos();
-    c.ball.dampVelocity(0.99);
-    c.ball.detectCollision(data.boardController.board);
-  });
+      c.ball.updatePos();
+      c.ball.dampVelocity(0.99);
+      c.ball.detectCollision(data.boardController.board);
+    });
 
-  requestAnimationFrame(() => draw(data));
+    requestAnimationFrame(() => draw(data));
+  }
 }
